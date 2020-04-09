8673302429af63828d8c883e4d304427e2bd5650
dotcs <git@dotcs.me>
2020-04-09 16:13:30 +0200

    Add missing files to PyPi package

--- setup.py
+++ setup.py
@@ -16,6 +16,7 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="",
     packages=setuptools.find_packages(),
+    include_package_data=True,
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
