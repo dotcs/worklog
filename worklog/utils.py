6070b9b669b116006621a9d26bbecf48c9c71ae1
dotcs <git@dotcs.me>
2020-08-06 08:26:56 +0200

    Fix order of config files

--- worklog/utils.py
+++ worklog/utils.py
@@ -11,8 +11,8 @@ LOG_FORMAT: str = logging.BASIC_FORMAT
 LOG_LEVELS: List[int] = [logging.ERROR, logging.WARN, logging.INFO, logging.DEBUG]
 
 CONFIG_FILES: List[str] = [
-    os.path.expanduser("~/.config/worklog/config"),
     os.path.join(os.path.abspath(os.path.dirname(__file__)), "config.cfg"),
+    os.path.expanduser("~/.config/worklog/config"),
 ]
 
 LOCAL_TIMEZONE: Optional[tzinfo] = datetime.now(timezone.utc).astimezone().tzinfo
