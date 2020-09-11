393f23b037d1c9c29d6925697f2a3b25c8cbe8cf
dotcs <git@dotcs.me>
2020-09-11 09:14:28 +0200

    Split doctor test function into multiple functions

--- worklog/tests/test_log.py
+++ worklog/tests/test_log.py
@@ -10,7 +10,14 @@ from worklog.errors import ErrMsg
 import worklog.constants as wc
 
 
-class TestInit(unittest.TestCase):
+class SnapshotMixin(object):
+    def _get_snapshot_fp(self, name):
+        return (
+            Path("worklog", "tests", "snapshots", f"{name}.csv").absolute().as_posix()
+        )
+
+
+class TestInit(unittest.TestCase, SnapshotMixin):
     def test_file_created(self):
         with tempfile.TemporaryDirectory() as tmpdir:
             fp = Path(tmpdir, "foobar")
@@ -27,28 +34,51 @@ class TestInit(unittest.TestCase):
             self.assertTrue(os.access(fp, os.W_OK))
 
     def test_file_is_read(self):
-        fp = (
-            Path("worklog", "tests", "snapshots", "session_simple.csv")
-            .absolute()
-            .as_posix()
-        )
+        fp = self._get_snapshot_fp("session_simple")
         instance = Log(fp)
         self.assertFalse(instance._log_df.empty)
 
 
-class TestDoctor(unittest.TestCase):
-    def test_session_only(self):
+class TestDoctorSession(unittest.TestCase, SnapshotMixin):
+    def test_ok(self):
         logger = logging.getLogger("test_logger")
         with patch.object(logger, "error") as mock_logger:
-            fp = (
-                Path("worklog", "tests", "snapshots", "doctor_session_only.csv")
-                .absolute()
-                .as_posix()
-            )
+            fp = self._get_snapshot_fp("doctor_session_ok")
+            instance = Log(fp, logger=logger)
+            instance.doctor()
+
+            mock_logger.assert_not_called()
+
+    def test_stop_entry_missing_single(self):
+        logger = logging.getLogger("test_logger")
+        with patch.object(logger, "error") as mock_logger:
+            fp = self._get_snapshot_fp("doctor_session_stop_missing_single")
             instance = Log(fp, logger=logger)
             instance.doctor()
 
             calls = (
+                call(
+                    ErrMsg.MISSING_SESSION_ENTRY.value.format(
+                        type=wc.TOKEN_STOP, date="2020-01-01"
+                    )
+                ),
+            )
+
+            mock_logger.assert_has_calls(calls)
+
+    def test_stop_entry_missing_multiple(self):
+        logger = logging.getLogger("test_logger")
+        with patch.object(logger, "error") as mock_logger:
+            fp = self._get_snapshot_fp("doctor_session_stop_missing_multiple")
+            instance = Log(fp, logger=logger)
+            instance.doctor()
+
+            calls = (
+                call(
+                    ErrMsg.MISSING_SESSION_ENTRY.value.format(
+                        type=wc.TOKEN_STOP, date="2020-01-01"
+                    )
+                ),
                 call(
                     ErrMsg.MISSING_SESSION_ENTRY.value.format(
                         type=wc.TOKEN_STOP, date="2020-01-02"
@@ -59,52 +89,105 @@ class TestDoctor(unittest.TestCase):
                         type=wc.TOKEN_STOP, date="2020-01-03"
                     )
                 ),
+            )
+
+            mock_logger.assert_has_calls(calls)
+
+    def test_wrong_order(self):
+        logger = logging.getLogger("test_logger")
+        with patch.object(logger, "error") as mock_logger:
+            fp = self._get_snapshot_fp("doctor_session_wrong_order")
+            instance = Log(fp, logger=logger)
+            instance.doctor()
+
+            calls = (call(ErrMsg.WRONG_SESSION_ORDER.value.format(date="2020-01-01")),)
+
+            mock_logger.assert_has_calls(calls)
+
+    def test_wrong_order_2(self):
+        logger = logging.getLogger("test_logger")
+        with patch.object(logger, "error") as mock_logger:
+            fp = self._get_snapshot_fp("doctor_session_wrong_order_2")
+            instance = Log(fp, logger=logger)
+            instance.doctor()
+
+            calls = (call(ErrMsg.WRONG_SESSION_ORDER.value.format(date="2020-01-01")),)
+
+            mock_logger.assert_has_calls(calls)
+
+
+class TestDoctorTask(unittest.TestCase, SnapshotMixin):
+    def test_ok(self):
+        logger = logging.getLogger("test_logger")
+        with patch.object(logger, "error") as mock_logger:
+            fp = self._get_snapshot_fp("doctor_task_ok")
+            instance = Log(fp, logger=logger)
+            instance.doctor()
+
+            mock_logger.assert_not_called()
+
+    def test_task_start_entry_missing(self):
+        logger = logging.getLogger("test_logger")
+        with patch.object(logger, "error") as mock_logger:
+            fp = self._get_snapshot_fp("doctor_task_start_missing")
+            instance = Log(fp, logger=logger)
+            instance.doctor()
+
+            calls = (
                 call(
-                    ErrMsg.MISSING_SESSION_ENTRY.value.format(
-                        type=wc.TOKEN_STOP, date="2020-01-04"
+                    ErrMsg.MISSING_TASK_ENTRY.value.format(
+                        type=wc.TOKEN_START, date="2020-01-01", task_id="task1"
                     )
                 ),
             )
 
             mock_logger.assert_has_calls(calls)
 
-    def test_session_with_tasks(self):
+    def test_task_stop_entry_missing(self):
         logger = logging.getLogger("test_logger")
         with patch.object(logger, "error") as mock_logger:
-            fp = (
-                Path("worklog", "tests", "snapshots", "doctor_session_with_tasks.csv")
-                .absolute()
-                .as_posix()
-            )
+            fp = self._get_snapshot_fp("doctor_task_stop_missing")
             instance = Log(fp, logger=logger)
             instance.doctor()
 
             calls = (
                 call(
-                    ErrMsg.MISSING_SESSION_ENTRY.value.format(
-                        type=wc.TOKEN_STOP, date="2020-01-03"
-                    )
-                ),
-                call(ErrMsg.WRONG_SESSION_ORDER.value.format(date="2020-01-07")),
-                call(ErrMsg.WRONG_SESSION_ORDER.value.format(date="2020-01-08")),
-                call(
                     ErrMsg.MISSING_TASK_ENTRY.value.format(
-                        type=wc.TOKEN_STOP, date="2020-01-04", task_id="task4"
+                        type=wc.TOKEN_STOP, date="2020-01-01", task_id="task1"
                     )
                 ),
-                call(
-                    ErrMsg.MISSING_TASK_ENTRY.value.format(
-                        type=wc.TOKEN_START, date="2020-01-05", task_id="task5"
-                    )
-                ),
+            )
+
+            mock_logger.assert_has_calls(calls)
+
+    def test_task_wrong_order(self):
+        logger = logging.getLogger("test_logger")
+        with patch.object(logger, "error") as mock_logger:
+            fp = self._get_snapshot_fp("doctor_task_wrong_order")
+            instance = Log(fp, logger=logger)
+            instance.doctor()
+
+            calls = (
                 call(
                     ErrMsg.WRONG_TASK_ORDER.value.format(
-                        date="2020-01-06", task_id="task6"
+                        date="2020-01-01", task_id="task1"
                     )
                 ),
+            )
+
+            mock_logger.assert_has_calls(calls)
+
+    def test_task_wrong_order_2(self):
+        logger = logging.getLogger("test_logger")
+        with patch.object(logger, "error") as mock_logger:
+            fp = self._get_snapshot_fp("doctor_task_wrong_order_2")
+            instance = Log(fp, logger=logger)
+            instance.doctor()
+
+            calls = (
                 call(
                     ErrMsg.WRONG_TASK_ORDER.value.format(
-                        date="2020-01-09", task_id="task9"
+                        date="2020-01-01", task_id="task1"
                     )
                 ),
             )
